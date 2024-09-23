091d9a99df0c5ff9bdb559a82228157aaa2b23f9
Carson Irons <ci7229@princeton.edu>
2024-09-23 16:05:55 -0400

    removed restricted keyword

--- str.h
+++ str.h
@@ -1,5 +1,5 @@
 size_t Str_getLength(const char *s);
-char *Str_copy(char * restrict s1, const char * restrict s2);
-char *Str_concat(char * restrict s1, const char * restrict s2);
+char *Str_copy(char * s1, const char * s2);
+char *Str_concat(char * s1, const char * s2);
 int Str_compare(const char * s1, const char *s2);
 char *Str_search(const char *s1, const char *s2);
