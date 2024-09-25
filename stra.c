551345675648a11398245259db14988dbfa76ee4
Carson Irons <ci7229@princeton.edu>
2024-09-25 09:57:49 -0400

    added stra search

--- stra.c
+++ stra.c
@@ -52,5 +52,22 @@ int Str_compare(const char * s1, const char *s2) {
 }
 
 char *Str_search(const char *s1, const char *s2) {
-    return strstr(s1, s2);
+    int index = 0;
+    int jdex;
+    assert(s1 != NULL && s2 != NULL);
+    while (s1[index] != '\0') {
+        while (s1[index] != s2[0] && s1[index] != '\0') {
+            index++;
+        }
+        jdex = index;
+        while (s1[jdex] == s2[jdex] && s1[jdex] != '\0' && s2[jdex] != '\0') {
+            jdex++;
+        }
+        if (s2[jdex] == '\0') 
+            return &s1[index];
+        else if (s1[jdex] == '\0') 
+            return (char*)NULL;
+        else 
+            index++;
+    }
 }
